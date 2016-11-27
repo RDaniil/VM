1f3bd8103bbf454f3ab74965dea89503d10bc556
RDaniil <danrumyantsev@yandex.ru>
2016-11-27 17:09:25 +0500

    Карринг команд, пошаговость.

--- Virtual Macine/proc.js
+++ Virtual Macine/proc.js
@@ -3,304 +3,183 @@ Proc = function(){
 	this.regsVal = [];
 	this.regsList = [];
 	this.IP = 0;
-	this.lables = [];
+	this.labels = [];
 	this.instrList = [];
 }
 
-/*Принимает масив операндов, их должно быть два:
-*(*регистр,регистр*)/(*регистр,число*)
-*(Первый ОБЯЗАТЕЛЬНО регистр),
-* складывает значение второго операнда с значением первого регистра.
-*/
-Proc.prototype.procADD = function(arrOP)
-{
+var procINSTR = function(labels){
+	return function(flags){
+		return function(regs){
+			return function(instr){
+				return function(operation){
+					return function(operators){
+						return instr(labels, flags, regs, operation, operators);
+					}
+				}
+			}
+		}
+	}
+}
+
+var procBinaryInstr = function(labels, flags, regs, operation, arrOP){
 	if(arrOP.length != 2){
-		this.flags["ERF"] = true;
-		alert("Error: 'ADD' takes 2 arguments!");
+		flags["ERF"] = true;
+		alert("Error: binary operation takes 2 arguments!");
 		return;
 	}
 	var op1 = arrOP[0];
 	var op2 = arrOP[1];
-
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-	var secondIsReg = this.regsList.some( function(item){ return item == op2; } ) //Является ли второй операнд регистром
-
-	if(firstIsReg){
-		if(secondIsReg){ //Если оба оператора - регистры, берем из второго, прибавляем к первому
-			this.regsVal[op1] += this.regsVal[op2];
-			return 0;
+	var isReg1Exist = false;
+	var isReg2Exist = false;
+	for(reg in regs){
+		if(reg == op1){
+			isReg1Exist = true;
 		}
-		if(Number(op2) != NaN){ //Если второй оператор число - прибавляем его к знач. регистра
-			this.regsVal[op1] += Number(op2);
-			return 0;
-		}
+		if(reg == op2){
+			isReg2Exist = true;
+		}	
 	}
 
-	alert("Unknown Error!!! Command - 'ADD' IP = "+ this.IP);
-}
-
-/* Работает аналогично ADD, только вычитает
-*/
-Proc.prototype.procSUB = function(arrOP)
-{
-	if(arrOP.length != 2){
-		this.flags["ERF"] = true;
-		alert("Error: 'SUB' takes 2 arguments!");
+	if(!isReg1Exist){
+		alert(op1 + " Must be a register!");
+		flags["ERF"] = true;
 		return;
 	}
-	var op1 = arrOP[0];
-	var op2 = arrOP[1];
-
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-	var secondIsReg = this.regsList.some( function(item){ return item == op2; } ) //Является ли второй операнд регистром
-
-	if(firstIsReg){
-		if(secondIsReg){ //Если оба оператора - регистры, берем из второго, прибавляем к первому
-			this.regsVal[op1] -= this.regsVal[op2];
-			return 0;
-		}
-		if(Number(op2) != NaN){ //Если второй оператор число - прибавляем его к знач. регистра
-			this.regsVal[op1] -= Number(op2);
-			return 0;
-		}
-	}
-
-	alert("Unknown Error!!! Command - 'SUB' IP = "+ this.IP);
-}
-
-/*Принимает масив операндов, их должно быть два:
-*(*регистр,регистр*)/(*регистр,число*)
-*(Первый ОБЯЗАТЕЛЬНО регистр),
-*Умножает первое на второе
-*/
-Proc.prototype.procMUL = function(arrOP)
-{
-	if(arrOP.length != 2){
-		this.flags["ERF"] = true;
-		alert("Error: 'MUL' takes 2 arguments!");
+	if(isReg2Exist){
+		var operators = [regs[op1], regs[op2]];
+		regs[op1] = operation(operators);
 		return;
 	}
-	var op1 = arrOP[0];
-	var op2 = arrOP[1];
-
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-	var secondIsReg = this.regsList.some( function(item){ return item == op2; } ) //Является ли второй операнд регистром
-
-	if(firstIsReg){
-		if(secondIsReg){ //Если оба оператора - регистры, первый умножаем на второй
-			this.regsVal[op1] *= this.regsVal[op2];
-			return 0;
-		}
-		if(Number(op2) != NaN){ //Если второй оператор число - умножаем знач. регистра на него
-			this.regsVal[op1] *= Number(op2);
-			return 0;
-		}
-	}
-
-	alert("Unknown Error!!! Command - 'MUL' IP = "+ this.IP);
-}
-
-/*Принимает масив операндов, их должно быть два:
-*(*регистр,регистр*)/(*регистр,число*)
-*(Первый ОБЯЗАТЕЛЬНО регистр),
-*Делит первое на второе
-*/
-Proc.prototype.procDIV = function(arrOP)
-{
-	if(arrOP.length != 2){
-		this.flags["ERF"] = true;
-		alert("Error: 'DIV' takes 2 arguments!");
+	if(Number(op2) != NaN){
+		var operators = [regs[op1], op2];
+		regs[op1] = operation(operators);
 		return;
 	}
-	var op1 = arrOP[0];
-	var op2 = arrOP[1];
-
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-	var secondIsReg = this.regsList.some( function(item){ return item == op2; } ) //Является ли второй операнд регистром
-
-	if(firstIsReg){
-		if(secondIsReg){ //Если оба оператора - регистры, первый делим на второй
-			this.regsVal[op1] /= this.regsVal[op2];
-			return 0;
-		}
-		if(Number(op2) != NaN){ //Если второй оператор число - делим знач. регистра на него
-			this.regsVal[op1] /= Number(op2);
-			return 0;
-		}
-	}
-
-	alert("Unknown Error!!! Command - 'DIV' IP = "+ this.IP);
+	alert("Unknown error!! binary instr");
+	flags["ERF"] = true;
 }
 
-/*Принимает регистр, увеличивает значение на 1
-*/
-Proc.prototype.procINC = function(arrOP)
-{
+var procUnaryInstr = function(labels, flags, regs, operation, arrOP){
 	if(arrOP.length != 1){
-		this.flags["ERF"] = true;
-		alert("Error: 'INC' takes 1 argument!");
+		flags["ERF"] = true;
+		alert("Error: unary operation takes 1 arguments!");
 		return;
 	}
-
 	var op1 = arrOP[0];
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-
-	if(firstIsReg){
-			this.regsVal[op1] += 1;
-			return 0;
+	var isReg1Exist = false;
+	for(reg in regs){
+		if(reg == op1){
+			isReg1Exist = true;
+		}	
 	}
 
-	alert("Operand is not register!!! Command - 'INC'");
-}
-
-/*Принимает регистр, уменьшает значение на 1
-*/
-Proc.prototype.procDEC = function(arrOP)
-{
-	if(arrOP.length != 1){
-		this.flags["ERF"] = true;
-		alert("Error: 'DEC' takes 1 argument!");
+	if(!isReg1Exist){
+		alert(op1 + " Must be a register!");
+		flags["ERF"] = true;
 		return;
 	}
-
-	var op1 = arrOP[0];
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-
-	if(firstIsReg){
-			this.regsVal[op1] -= 1;
-			return 0;
+	if(isReg1Exist){
+		var operators = [regs[op1]];
+		regs[op1] = operation(operators);
+		return;
 	}
-
-	alert("Operand is not register!!! Command - 'DEC'");
+	alert("Unknown error!! unary instr");
+	flags["ERF"] = true;
 }
 
-/*Принимает масив операндов, кладет в 
-*первый регистр значние второго операнда
-*/
-Proc.prototype.procMOV = function(arrOP)
-{
+var procCompareInstr = function(labels, flags, regs, operation, arrOP){
 	if(arrOP.length != 2){
-		this.flags["ERF"] = true;
-		alert("Error: 'MOV' takes 2 argument!");
-		return;
-	}
-
-	var op1 = arrOP[0];
-	var op2 = arrOP[1];
-
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-	var secondIsReg = this.regsList.some( function(item){ return item == op2; } ) //Является ли второй операнд регистром
-
-	if(firstIsReg){
-		if(secondIsReg){ //Если оба оператора - регистры присваиваем знач. 2регистра 1регистру
-			this.regsVal[op1] = this.regsVal[op2];
-			return 0;
-		}
-		if(Number(op2) != NaN){ //Если второй оператор число - кладем его в регистр
-			this.regsVal[op1] = Number(op2);
-			return 0;
-		}
-	}
-
-	alert("Unknown Error!!! Command - 'MOV' IP = "+ this.IP);
-}
-
-//Делает из отрицательного положительное
-Proc.prototype.procPOS = function(arrOP)
-{
-	if(arrOP.length != 1){
-		this.flags["ERF"] = true;
-		alert("Error: 'POS' takes 1 argument!");
-		return;
-	}
-
-	var op1 = arrOP[0];
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-
-	//Если значение в регисре меньше нуля, то делаем положителным, иначе ничего неделаем
-	if(firstIsReg){
-		if(this.regsVal[op1] < 0){
-			this.regsVal[op1] = -this.regsVal[op1];
-			return 0;
-		}
-	}
-
-	alert("Unknown Error!!! Command - 'POS' IP = "+ this.IP);
-}
-
-//Делает из положительного отрицаельное
-Proc.prototype.procNEG = function(arrOP)
-{
-	if(arrOP.length != 1){
-		this.flags["ERF"] = true;
-		alert("Error: 'NEG' takes 1 argument!");
-		return;
-	}
-
-	var op1 = arrOP[0];
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-
-	if(firstIsReg){
-		if(this.regsVal[op1] > 0){
-			this.regsVal[op1] = (-this.regsVal[op1]);
-			return 0;
-		}
-	}
-
-	alert("Unknown Error!!! Command - 'NEG' IP = "+ this.IP);
-}
-
-
-Proc.prototype.procCOMPARES = function(arrOP)
-{
-	if(arrOP.length != 2){
-		this.flags["ERF"] = true;
-		alert("Error: 'CO?' takes 2 arguments!");
+		flags["ERF"] = true;
+		alert("Error: Compare operation takes 2 arguments!");
 		return;
 	}
 	var op1 = arrOP[0];
 	var op2 = arrOP[1];
-
-	var firstIsReg  = this.regsList.some( function(item){ return item == op1; } ) //Является ли первый операнд регистром
-	var secondIsReg = this.regsList.some( function(item){ return item == op2; } ) //Является ли второй операнд регистром
-
-	if(firstIsReg){
-		if(secondIsReg){ //Если оба оператора - регистры, первый делим на второй
-			this.regsVal[op1] /= this.regsVal[op2];
-			return 0;
+	var isReg1Exist = false;
+	var isReg2Exist = false;
+	for(reg in regs){
+		if(reg == op1){
+			isReg1Exist = true;
 		}
-		if(Number(op2) != NaN){ //Если второй оператор число - делим знач. регистра на него
-			this.regsVal[op1] /= Number(op2);
-			return 0;
+		if(reg == op2){
+			isReg2Exist = true;
+		}	
+	}
+
+	if(!isReg1Exist){
+		alert(op1 + " Must be a register!");
+		flags["ERF"] = true;
+		return;
+	}
+	if(isReg2Exist){
+		var operators = [regs[op1], regs[op2]];
+		var cmpResult = operation(operators);
+	}else{
+		if(Number(op2) != NaN){
+			var operators = [regs[op1], op2];
+			var cmpResult = operation(operators);
+		}
+	}
+	flags["CMPEF"] = cmpResult[0];
+	flags["CMPUF"] = cmpResult[1];
+	flags["CMPGF"] = cmpResult[2];
+	flags["CMPLF"] = cmpResult[3];
+	return;
+}
+
+var procJumpInstr = function(labels, flags, regs, operation, arrOP){
+	if(arrOP.length != 1){
+		flags["ERF"] = true;
+		alert("Error: Jumps operations takes 1 arguments!");
+		return;
+	}
+	
+	var label = arrOP[0];
+	console.log("Label in Jump is: " + label);
+
+	var isLabelExist = false;
+	for(l in labels){
+		if(l == label){
+			isLabelExist = true;
 		}
 	}
 
-	alert("Unknown Error!!! Command - 'DIV' IP = "+ this.IP);
+	if(!isLabelExist){
+		alert(label + " This label is not exists");
+		flags["ERF"] = true;
+		return -1;
+	}
+
+	//Если условия для прыжка выполнены, то возвращается номер строки метки
+	if(operation(flags)){
+		console.log("Label " + label + " EXIST !! Label number is " + labels[label]);
+		return labels[label];
+	}else return -1;
 }
 
-Proc.prototype.initInstructions = function()
+Proc.prototype.initInstructionList = function()
 {
-	/*this.instr["ADD"] = this.procADD;
-	this.instr["DEC"] = this.procDEC;
-	this.instr["INC"] = this.procINC;
-	this.instr["SUB"] = this.procSUB;
-	this.instr["MOV"] = this.procMOV;*/
 	this.instrList.push("MOV");//MOVE
-
 	this.instrList.push("ADD");//ADD
-	this.instrList.push("DEC");//DECREMENT
-	this.instrList.push("INC");//INCREMENT
 	this.instrList.push("SUB");//SUBSTRACT
 	this.instrList.push("MUL");//MULTIPLY
 	this.instrList.push("DIV");//DIVIDE
+	this.instrList.push("RDV");//REST of DIVIIDING
+
+	this.instrList.push("DEC");//DECREMENT
+	this.instrList.push("INC");//INCREMENT
 	this.instrList.push("NEG");//to NEGATIVE
 	this.instrList.push("POS");//to POSITIVE
 
-	this.instrList.push("COU");//COMPARE UNEQUAL
-	this.instrList.push("COG");//COMPARE GREATER
-	this.instrList.push("COL");//COMPARE LESSER
-	this.instrList.push("COE");//COMPARE EQUAL
+	this.instrList.push("CMP");//COMPARE 
 	this.instrList.push("JMP");//JUMP to
+	this.instrList.push("JPU");//JUMP if UNEQUAL
+	this.instrList.push("JPE");//JUMP if EQUAL
+	this.instrList.push("JPL");//JUMP if LESSER
+	this.instrList.push("JPG");//JUMP if GREATER
+
+	this.instrList.push("EXT");//EXIT
+
 }
 
 Proc.prototype.initRegsisters = function(nRegisters)
@@ -314,24 +193,65 @@ Proc.prototype.initRegsisters = function(nRegisters)
 
 Proc.prototype.initFlags = function()
 {
-	this.flags["ICF"] = false; //In Condition Flag - true - находимся в блоке с условием
-	this.flags["CVF"] = false; //Condition Value Flag - true - условие верно, false - не верно
-	this.flags["ERF"] = false; //Error Flag - true если была совершена ошибка в синтаксисе
+	//Compare Unequal Flag - true - сравнение показало, что операнды не равны
+	this.flags["CMPUF"] = false;
+	//Compare Equal Flag - true - сравнение показало, что операнды равны 
+	this.flags["CMPEF"] = false;
+	//Compare Lesser Flag - true - сравнение показало, что оп1 меньше чем оп2 
+	this.flags["CMPLF"] = false;
+	//Compare Greater Flag - true - сравнение показало, что оп1 больше чем оп2 
+	this.flags["CMPGF"] = false;
+	//Error Flag - true если была совершена ошибка в синтаксисе
+	this.flags["ERF"] = false; 
+}
+
+Proc.prototype.initInstructions = function(nRegisters)
+{
+	//null потому что метки не нужны в этих операциях
+	var binInstrCurry = procINSTR(null)(this.flags)(this.regsVal)(procBinaryInstr);
+	Proc.prototype.procADD = binInstrCurry(ADD);
+	Proc.prototype.procSUB = binInstrCurry(SUB);
+	Proc.prototype.procMUL = binInstrCurry(MUL);
+	Proc.prototype.procDIV = binInstrCurry(DIV);
+	Proc.prototype.procRDV = binInstrCurry(RDV);
+
+	//null потому что метки не нужны в этих операциях
+	var unaryInstrCurry = procINSTR(null)(this.flags)(this.regsVal)(procUnaryInstr);
+	Proc.prototype.procINC = unaryInstrCurry(INC);
+	Proc.prototype.procDEC = unaryInstrCurry(DEC);
+	Proc.prototype.procPOS = unaryInstrCurry(POS);
+	Proc.prototype.procNEG = unaryInstrCurry(NEG);
+
+	var cmpInstrCurry = procINSTR(null)(this.flags)(this.regsVal)(procCompareInstr);
+	Proc.prototype.procCMP = cmpInstrCurry(CMP);
+
+	//null потому что здесь не нужны регистры
+	var jumpInstrCurry = procINSTR(this.labels)(this.flags)(null)(procJumpInstr);
+	Proc.prototype.procJMP = jumpInstrCurry(JMP);
+	Proc.prototype.procJPU = jumpInstrCurry(JPU);
+	Proc.prototype.procJPE = jumpInstrCurry(JPE);
+	Proc.prototype.procJPL = jumpInstrCurry(JPL);
+	Proc.prototype.procJPG = jumpInstrCurry(JPG);
+
 }
 
 Proc.prototype.initProc = function(nRegisters)
 {
 	this.initRegsisters(nRegisters);
 	this.initFlags();
+	this.initInstructionList();
 	this.initInstructions();
 }
 
-/*Метки - ассоциативный масив: ключ-метка, 
-*значние-номер строки, на которой расположена метка
-*/
-Proc.prototype.addLable = function(lable)
-{
-	this.lables[lable] = this.IP;
+Proc.prototype.getLabels = function(codeStrings)
+{	
+	for (var i = 0; i < codeStrings.length; i++) {
+		var code = codeStrings[i].split(":");
+		if(code.length > 1){
+			this.labels[code[0]] = i;
+			console.log("Getting Label: " + code[0] + ", label number: " + i);
+		}
+	}
 }
 
 /* Формат строки: label:_INSTR_op1,op2_;comment
@@ -343,21 +263,8 @@ Proc.prototype.readMemory = function(memory)
 	var formatMemStr = [];
 	formatMemStr = memStr.split(";"); //Сплит по ; разделит комментарий от кода
 	formatMemStr.pop();//Выкидываем комментарий
-	//console.log("Format mem str:"+formatMemStr[0]);
+	console.log("Processor read memory: " + formatMemStr[0]);
 	return formatMemStr;
-}
-
-Proc.prototype.isRegExist = function(arrOP)
-{
-	for (var i = 0; i < arrOP.length; i++) {
-		if(arrOP[i].charAt(0) == "r"){
-			if(arrOP[i].charAt(1) <= this.regsList.length)
-				return true;
-			alert("Invalid register name: "+ arrOP[i]);
-			this.flags["ERF"] = true;
-			return false;
-		}
-	};
 }
 
 /*Проверяется существование инструкции, наличие регистров
@@ -374,27 +281,18 @@ Proc.prototype.applyInstr = function(instr, OpStr)
 		return;
 	}
 
-	var arrOP = OpStr[0].split(',');//Операторы разделены запятой
-	if(!this.isRegExist(arrOP)){
-		return;
+	//Длина массива операндов мб равна 0, в случае операторов без аргументов напр. EXT
+	if(OpStr.length != 0){
+		var arrOP = OpStr[0].split(',');//Операторы разделены запятой
+		console.log("IP: " + this.IP + " Instr: " + instruction+ " operands: " + arrOP);
 	}
-
 	switch(instruction){
 		case "ADD":
 			this.procADD(arrOP);
 			this.IP++;
-			console.log("IN SWITCH"+ this.IP);
 			break
 		case "SUB":
 			this.procSUB(arrOP);
-			this.IP++;
-			break
-		case "INC":
-			this.procINC(arrOP);
-			this.IP++;
-			break
-		case "DEC":
-			this.procDEC(arrOP);
 			this.IP++;
 			break
 		case "MOV":
@@ -409,6 +307,55 @@ Proc.prototype.applyInstr = function(instr, OpStr)
 			this.procMUL(arrOP);
 			this.IP++;
 			break
+		case "RDV":
+			this.procRDV(arrOP);
+			this.IP++;
+			break
+		case "INC":
+			this.procINC(arrOP);
+			this.IP++;
+			break
+		case "DEC":
+			this.procDEC(arrOP);
+			this.IP++;
+			break	
+		case "POS":
+			this.procPOS(arrOP);
+			this.IP++;
+			break
+		case "NEG":
+			this.procNEG(arrOP);
+			this.IP++;
+			break
+		case "CMP":
+			this.procCMP(arrOP);
+			this.IP++;
+			break
+		case "JMP":
+			var res = this.procJMP(arrOP);
+			(res == -1) ? this.IP++ : (this.IP = res);
+			break
+		case "JPU":
+			var res = this.procJPU(arrOP);
+			(res == -1) ? this.IP++ : (this.IP = res);
+			break	
+		case "JPE":
+			var res = this.procJPE(arrOP);
+			(res == -1) ? this.IP++ : (this.IP = res);
+			break
+		case "JPL":
+			var res = this.procJPL(arrOP);
+			(res == -1) ? this.IP++ : (this.IP = res);
+			break
+		case "JPG":
+			var res = this.procJPG(arrOP);
+			(res == -1) ? this.IP++ : (this.IP = res);
+			break
+		//Костыль, для выхода меняем флаг ошибки на "true"
+		case "EXT":
+			console.log("IN EXIT");
+			this.flags["ERF"] =true;
+			break
 		default:
 			alert("There is no instr: "+instruction);
 			this.flags["ERF"]= true;
@@ -421,32 +368,19 @@ Proc.prototype.executeInstr = function(memStr)
 	var arrOfInstr = memStr.split(" ");
 	var isInstrDefined = this.instrList.some( function(item){ return item == arrOfInstr[0]; } )
 	if(!isInstrDefined){//Если первая инструкция не определенная команда, то это должна быть метка
-		var label = arrOfInstr.splice(0,1);//Первый эл.(метка в этом случае) выбрасывается
-		this.addLable(label); //И добавляеся в массив меток
+		console.log("Deleted : " + arrOfInstr[0]);
+		arrOfInstr.splice(0,1);//Первый эл.(метка в этом случае) выбрасывается
 	}
 	/*На этом этапе массив инструкий должен состоять только из
 	*инструкции и операнда(ов). В функцию выталкиывается
 	*инструкция, а оставшийся массив и есть операнды
-	*
-	*Если находимся в условии, необходимо проверить выполнено ли оно
-	*если да, выполняем инструкцию в стандартном режиме
-	*если нет, перепрыгиваем строку и выходим из этой функции
 	*/
-	if(this.flags["ICF"] == true){
-		if(this.flags["CVF"] == true){
-			applyInstr(arrOfInstr.splice(0,1), arrOfInstr);
-			return;
-		}else{ //Если условие не верное, то перепрыгиваем строку
-			this.IP++; 
-			return;
-		}
-	}
 	this.applyInstr(arrOfInstr.splice(0,1), arrOfInstr);
 }
 
 Proc.prototype.step = function(memStr)
 {
-	console.log("STEP " + memStr);
+	//console.log("STEP " + memStr);
 	this.executeInstr(memStr);
 }
 
---
+++ Virtual Macine/procINSTR.js
@@ -0,0 +1,56 @@
+//JumpInstructions
+function JMP(operators){
+	return true;
+}
+function JPU(operators){
+	return operators["CMPUF"] == true;
+}
+function JPE(operators){
+	return operators["CMPEF"] == true;
+}
+function JPL(operators){
+	return operators["CMPLF"] == true;
+}
+function JPG(operators){
+	return operators["CMPGF"] == true;
+}
+
+//CondiTionalInstrucions
+function CMP(ops){
+	var cmpRes= [];
+	(ops[0] == ops[1]) ? cmpRes.push(true) : cmpRes.push(false);
+	(ops[0] != ops[1]) ? cmpRes.push(true) : cmpRes.push(false);
+	(ops[0] > ops[1]) ? cmpRes.push(true) : cmpRes.push(false);
+	(ops[0] < ops[1]) ? cmpRes.push(true) : cmpRes.push(false);
+	return cmpRes;
+}
+//BinaryInstructions
+function ADD(operators){
+	return Number(operators[0]) + Number(operators[1]);
+}
+function SUB(operators){
+	return operators[0] - operators[1];
+}
+function MUL(operators){
+	return operators[0] * operators[1];
+}
+function DIV(operators){
+	return operators[0] / operators[1];
+}
+function RDV(operators){
+	return operators[0] % operators[1];
+}
+
+//UnaryInstructions
+function INC(operators){
+	return operators[0] + 1;
+}
+function DEC(operators){
+	return operators[0] - 1;
+}
+function POS(operators){
+	return (operators[0]>0) ? operators[0] : (-operators[0]);
+}
+function NEG(operators){
+	return (operators[0]<0) ? operators[0] : (-operators[0]);
+}
